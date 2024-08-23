7932894ca52fba695a305fe11e2ab814f0799f4c
Gustavo Lima <gustavolimamendes2@gmail.com>
2024-08-22 21:14:57 -0300

    feat: ninja controller

---
+++ src/main/java/dev/gustavo/ApiNaruto/controller/NinjasController.java
@@ -0,0 +1,11 @@
+package dev.gustavo.ApiNaruto.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/ninjas")
+
+public class NinjasController {
+
+}
--- src/main/java/dev/gustavo/ApiNaruto/service/NinjasServices.java
+++ src/main/java/dev/gustavo/ApiNaruto/service/NinjasServices.java
@@ -1,5 +1,6 @@
 package dev.gustavo.ApiNaruto.service;
 
+import dev.gustavo.ApiNaruto.model.Ninjas;
 import dev.gustavo.ApiNaruto.repository.NinjasRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -8,5 +9,9 @@ import org.springframework.stereotype.Service;
 public class NinjasServices {
     /*TODO: Refatorar a instância*/
     @Autowired
-    private NinjasRepository repo;
+    private NinjasRepository repository;
+
+    public Ninjas addNinja(Ninjas ninja) {
+        return repository.save(ninja);
+    }
 }
